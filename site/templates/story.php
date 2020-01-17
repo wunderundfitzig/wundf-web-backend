62e9e403021cf3f63aebf324a73bb0cee7a3ef75
Manuel Reich <mr@wundf.net>
2020-01-17 18:47:14 +0100

    Allow even more editor blocks

--- site/templates/story.php
+++ site/templates/story.php
@@ -3,18 +3,13 @@
 $kirby->response()->json();
 
 $content = [];
+$tempListPart = '';
 foreach($page->main_content()->blocks() as $block):
   $type = (string)$block->type();
   switch ($type) {
     case "kirbytext":
       $content[] = array(
         'type' => 'text',
-        'text' => (string)$block->content()
-      );
-    break;
-    case "paragraph":
-      $content[] = array(
-        'type' => 'html',
         'text' => (string)$block->content()
       );
     break;
@@ -34,10 +29,28 @@ foreach($page->main_content()->blocks() as $block):
     break;
     case "image":
       $content[] = array(
-        'type' => $type,
+        'type' => 'image',
         'url' => (string)$block->attrs()->src(),
         'caption' => (string)$block->attrs()->caption(),
         'alt' => (string)$block->attrs()->alt()
+      );
+    break;
+    case 'ul':
+      if ($block->hasNext() && $block->next()->type() === 'ul') {
+        $tempListPart .= $block->html();
+        break;
+      }
+
+      $content[] = array(
+        'type' => 'html',
+        'html' => $tempListPart . $block->html()
+      );
+      $tempListPart = '';
+    break;
+    default:
+      $content[] = array(
+        'type' => 'html',
+        'html' => (string)$block->html()
       );
     break;
   }
